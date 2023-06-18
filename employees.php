cb88211b231ff3d9c93eed816a305f931f5c491d
Fathyy <fathiabdi254@gmail.com>
2023-06-18 21:39:42 +0300

    minor changes

---
+++ employees.php
@@ -0,0 +1,50 @@
+<?php 
+
+include ("index.php");
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Full Name</th>
+                        <th>Department</th>
+                        <th>Salary</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    require __DIR__ . "/config/database.php";
+                    $sql = "SELECT * FROM employees";
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) :
+                        while ($row = mysqli_fetch_assoc($result)) : ?>
+                            <tr>
+                                <td><?php echo $row['Full Name']?></td>
+                                <td><?php echo $row['Department']?></td>
+                                <td><?php echo $row['Salary']?></td> 
+                                <td>A</td>
+                            </tr>
+                        <?php endwhile ?>
+                    <?php endif?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
