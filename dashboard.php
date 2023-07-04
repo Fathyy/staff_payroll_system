238bfa87e904c8f20673d2283090acf56df30394
Fathyy <fathiabdi254@gmail.com>
2023-07-04 11:07:01 +0300

    the dashboard.php looks better

--- dashboard.php
+++ dashboard.php
@@ -1,34 +1,64 @@
-<?php require __DIR__ . '/includes/navbar.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php require __DIR__ . '/includes/navbar.php'?>
+<div class="container">
+    <div class="row mt-5">
 
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card mb-3" style="max-width: 18rem;">
-                <div class="card-header">Department</div>
+        <div class="col-md-4" style="width:25%;">
+            <div class="card mb-3">
+                <div class="card-header">Departments</div>
                 <div class="card-body">
                     <h1 class="card-text">
-                        <?php require 'config/database.php'
-                        $sql = "SELECT ";
+                        <?php require 'config/database.php';
+                        $sql = "SELECT id from department";
+                        $result = mysqli_query($conn, $sql);
+                        echo mysqli_affected_rows($conn);
                         ?>
                     </h1>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-4">
-            <div class="card mb-3" style="max-width: 18rem;">
+        <div class="col-md-4" style="width:25%;">
+            <div class="card mb-3">
                 <div class="card-header">Employees</div>
                 <div class="card-body">
-                    <h1 class="card-text"></h1>
+                    <h1 class="card-text">
+                        <?php require 'config/database.php';
+                        $sql = "SELECT id from employees";
+                        $result = mysqli_query($conn, $sql);
+                        echo mysqli_affected_rows($conn);
+                        ?>
+                    </h1>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-4">
-            <div class="card mb-3" style="max-width: 18rem;">
+        <div class="col-md-4" style="width:25%;">
+            <div class="card mb-3">
                 <div class="card-header">Payslip</div>
                 <div class="card-body">
-                    <h1 class="card-text"></h1>
+                    <h1 class="card-text">
+                        <?php require 'config/database.php';
+                        // change this one to payslip later
+                        $sql = "SELECT id from department";
+                        $result = mysqli_query($conn, $sql);
+                        echo mysqli_affected_rows($conn);
+                        ?>
+                    </h1>
                 </div>
             </div>
         </div>
+
     </div>
+</div>
+</body>
+</html>
+
+    
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -6,7 +6,11 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/ed20622ed8.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/sidebar.css">
+    <style>
+        .nav-pills li a:hover{
+        background-color: blue;
+    }
+    </style>
   </head>
   <body>
     <div class="container-fluid">
@@ -59,7 +63,8 @@
             </div>
 
             <div class="content">
-              <?php include 'navbar.php'?>
+                <?php include 'dashboard.php'?>
+              
           </div>
         </div>
     </div>
