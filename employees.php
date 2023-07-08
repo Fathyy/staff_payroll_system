e808a7747ae853a4a32325cb2f56f1fc96f49fd7
Fathyy <fathiabdi254@gmail.com>
2023-07-08 22:11:31 +0300

    employees

--- employees.php
+++ employees.php
@@ -1,63 +1,109 @@
 <?php
 // The user should be logged in to access this page
-session_start();
-if (!isset($_SESSION['auth'])) {
-    header("Location: login.php");
-    exit;
-}
+// session_start();
+// if (!isset($_SESSION['auth'])) {
+//     header("Location: login.php");
+//     exit;
+// }
+?>
 
-require __DIR__ . '/includes/navbar.php';
-require __DIR__ . '/includes/header.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/ed20622ed8.js" crossorigin="anonymous"></script>
+    
+    <!-- Ajax jquery cdn -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <!-- datatables css cdn -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" />
+    <!-- datatables cdn -->
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
 
+    <!-- initialize datatables -->
+    <script>
+        $(document).ready( function () {
+        $('#myTable').DataTable();
+    } );
+    </script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="card" style="width:80%;">
+                    <div class="card-header">
+                        <span><b>Employee List</b></span>
+						<button class="btn btn-primary btn-sm btn-block col-md-3 float-end" type="button" id="new_attendance_btn">
+                            <span class="fa fa-plus"></span> Add Attendance
+                        </button>
+                    </div>
+                    <div class="card-body">
+                    
+                        <table class="table table-bordered table-hover table-responsive" id="table">
+                            <colgroup>
+								<col width="20%">
+								<col width="20%">
+								<col width="20%">
+								<col width="20%">
+								<col width="20%">
+							</colgroup>
+                            <thead>
+                                <tr>
+                                    <th>Full Name</th>
+                                    <th>Department</th>
+                                    <th>Designation</th>
+                                    <th>Salary</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                require __DIR__ . "/config/database.php";
+                                $sql = "SELECT * FROM employees";
+                                $result = mysqli_query($conn, $sql);
+                                if (mysqli_num_rows($result) > 0) :
+                                    while ($row = mysqli_fetch_assoc($result)) : ?>
+                                        <tr>
+                                            <td><?php echo $row['Full Name']?></td> 
+                                            <td><?php echo $row['Department']?></td>
+                                            <td><?php echo $row['Designation']?></td>
+                                            <td><?php echo $row['Salary']?></td> 
+                                            <td>
+                                                <!-- Edit -->
+                                                <a href="#?id=<?php echo htmlspecialchars($row['id'])?>" 
+                                                data-bs-toggle="tooltip" data-bs-title="Edit Record">
+                                                <i class="fa-regular fa-pen-to-square"></i></a>
 
-<div class="container">
-    <div class="row mt-5">
-        <div class="col-md-12">
-            <table class="table table-bordered table-hover table-responsive" style="width:80%;">
-                <thead>
-                    <tr>
-                        <th>Full Name</th>
-                        <th>Department</th>
-                        <th>Salary</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    require __DIR__ . "/config/database.php";
-                    $sql = "SELECT * FROM employees";
-                    $result = mysqli_query($conn, $sql);
-                    if (mysqli_num_rows($result) > 0) :
-                        while ($row = mysqli_fetch_assoc($result)) : ?>
-                            <tr>
-                                <td><?php echo $row['Full Name']?></td> 
-                                <td><?php echo $row['Department']?></td>
-                                <td><?php echo $row['Salary']?></td> 
-                                <td>
-                                    <!-- Edit -->
-                                    <a href="#?id=<?php echo htmlspecialchars($row['id'])?>" 
-                                    data-bs-toggle="tooltip" data-bs-title="Edit Record">
-                                    <i class="fa-regular fa-pen-to-square"></i></a>
+                                                <!-- Delete Record -->
+                                                <a href="#?id=<?php echo htmlspecialchars($row['id'])?>"
+                                                data-bs-toggle="tooltip" data-bs-title="Delete Record">
+                                                <i class="fa-solid fa-trash"></i></a>
 
-                                    <!-- Delete Record -->
-                                    <a href="#?id=<?php echo htmlspecialchars($row['id'])?>"
-                                    data-bs-toggle="tooltip" data-bs-title="Delete Record">
-                                    <i class="fa-solid fa-trash"></i></a>
-
-                                    <!-- the view link below shows a tooltip and opens a modal to show user details -->
-                                    <!-- View Record -->
-                                    <a href="" class="btn"
-                                    data-bs-toggle="tooltip" data-bs-title="View Record" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    <i class="fa-regular fa-eye"></i></a>
-                                </td>
-                            </tr>
-                        <?php endwhile ?>
-                    <?php endif?>
-                </tbody>
-            </table>
+                                                <!-- the view link below shows a tooltip and opens a modal to show user details -->
+                                                <!-- View Record -->
+                                                <a href="" class="btn"
+                                                data-bs-toggle="tooltip" data-bs-title="View Record" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                <i class="fa-regular fa-eye"></i></a>
+                                            </td>
+                                        </tr>
+                                    <?php endwhile ?>
+                                <?php endif?>
+                            </tbody>
+                        </table>
+            
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+
+
+    
+    
 
 <!-- modal to view user information -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="false" data-bs-keyboard="false">
@@ -77,9 +123,10 @@ require __DIR__ . '/includes/header.php';?>
     </div>
   </div>
 </div>
+   
 
-<!-- Ajax jquery cdn -->
-<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+
+
 <script>
     // open the modal
     $(document).ready(function(){
@@ -90,6 +137,15 @@ require __DIR__ . '/includes/header.php';?>
         });
     });
 </script>
-<?php require __DIR__ . '/includes/footer.php';?>
+</body>
+</html>
+
+
+
+
+
+
+
+
 
 
